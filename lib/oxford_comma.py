f4ac4bfcec82e5a3b28dc21a08a6bb481dda70a5
Gloria Jeyech <gjeyech@gmail.com>
2024-10-05 09:33:02 +0300

    oxford

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]  
+    elif len(items) == 2:
+        return " and ".join(items)  
+    elif len(items) == 3:
+        return f"{items[0]}, {items[1]}, and {items[2]}"  
+    else:
+        return ", ".join(items[:-1]) + ", and " + items[-1]
